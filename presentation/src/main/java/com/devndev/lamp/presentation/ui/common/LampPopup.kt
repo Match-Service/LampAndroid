9dc93b3bee73150579d9c61101779770c83f677a
soohwan-k <newpoo@naver.com>
2024-10-04 19:38:38 +0900

    feat:회원가입 동의 화면 팝업 구현

---
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/common/LampPopup.kt
@@ -0,0 +1,69 @@
+package com.devndev.lamp.presentation.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.devndev.lamp.presentation.ui.theme.Gray
+import com.devndev.lamp.presentation.ui.theme.Gray3
+import com.devndev.lamp.presentation.ui.theme.Typography
+
+@Composable
+fun OneButtonPopup(onDismissRequest: () -> Unit) {
+    Dialog(
+        onDismissRequest = { onDismissRequest() }
+    ) {
+        Column(
+            modifier = Modifier
+                .width(300.dp)
+                .background(Gray, shape = RoundedCornerShape(15.dp))
+                .padding(top = 20.dp, bottom = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                // todo 추후 팝업 내용 수정 서버에서 어떻게 받느냐에 따라 달라짐
+                Column() {
+                    Text(text = "목적", color = Color.White, style = Typography.medium15)
+                    Text(text = "행사 및 마케팅 활동", color = Gray3, style = Typography.normal12)
+                }
+                Column() {
+                    Text(text = "항목", color = Color.White, style = Typography.medium15)
+                    Text(text = "이메일", color = Gray3, style = Typography.normal12)
+                }
+                Column() {
+                    Text(text = "보유 및 이용기간", color = Color.White, style = Typography.medium15)
+                    Text(
+                        text = "정보제공 동의일로부터 회원탈퇴 및 동의 철회 시",
+                        color = Gray3,
+                        style = Typography.normal12
+                    )
+                }
+            }
+            HorizontalDivider(thickness = 1.dp, color = Gray3)
+            Text(
+                text = "닫기",
+                color = Color.White,
+                style = Typography.medium18,
+                modifier = Modifier.clickable { onDismissRequest() }
+            )
+        }
+    }
+}
