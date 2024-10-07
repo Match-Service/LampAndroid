0019471357cf628e3ff62cd61771fc04212fa644
soohwan-k <newpoo@naver.com>
2024-10-07 20:07:33 +0900

    feat:친구 초대 화면 친구 검색 임시 서버 연결

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/search/SearchViewModel.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/search/SearchViewModel.kt
@@ -3,31 +3,19 @@ package com.devndev.lamp.presentation.ui.search
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.devndev.lamp.domain.model.Item
 import com.devndev.lamp.domain.model.UserDomainModel
-import com.devndev.lamp.domain.usecase.ItemUseCase
 import com.devndev.lamp.domain.usecase.SearchUserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val itemUseCase: ItemUseCase,
     private val searchUserUseCase: SearchUserUseCase
 ) : ViewModel() {
 
-    private val _items = MutableStateFlow<List<Item>>(emptyList())
-    val items: StateFlow<List<Item>> = _items
-
     private val _users = mutableStateListOf<UserDomainModel>()
     val users: List<UserDomainModel> = _users
-
-    init {
-        fetchData()
-    }
 
     fun searchUsers(name: String) {
         viewModelScope.launch {
@@ -38,11 +26,5 @@ class SearchViewModel @Inject constructor(
 
     fun resetUsers() {
         _users.clear()
-    }
-
-    private fun fetchData() {
-        viewModelScope.launch {
-            _items.value = itemUseCase()
-        }
     }
 }
