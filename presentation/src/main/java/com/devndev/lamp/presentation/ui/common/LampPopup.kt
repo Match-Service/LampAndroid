1c8f79a9b0446522f47f5011e5bf6f65dc6f3179
soohwan-k <newpoo@naver.com>
2024-10-12 16:06:13 +0900

    fix:동의화면 팝업 버튼 영역 크기 수정

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/common/LampPopup.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/common/LampPopup.kt
@@ -2,18 +2,23 @@ package com.devndev.lamp.presentation.ui.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.devndev.lamp.presentation.ui.theme.Gray
@@ -37,7 +42,7 @@ fun OneButtonPopup(onDismissRequest: () -> Unit) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 20.dp),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
+                verticalArrangement = Arrangement.spacedBy(10.dp),
             ) {
                 // todo 추후 팝업 내용 수정 서버에서 어떻게 받느냐에 따라 달라짐
                 Column() {
@@ -57,13 +62,21 @@ fun OneButtonPopup(onDismissRequest: () -> Unit) {
                     )
                 }
             }
-            HorizontalDivider(thickness = 1.dp, color = Gray3)
-            Text(
-                text = "닫기",
-                color = Color.White,
-                style = Typography.medium18,
-                modifier = Modifier.clickable { onDismissRequest() }
-            )
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+                HorizontalDivider(thickness = 1.dp, color = Gray3)
+                Text(
+                    text = "닫기",
+                    color = Color.White,
+                    style = Typography.medium18,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable(
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ) { onDismissRequest() }
+                )
+            }
         }
     }
 }
