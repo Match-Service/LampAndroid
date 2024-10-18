f678427d9ec5dd414eed3a92ad137c9466933e8f
soohwan-k <newpoo@naver.com>
2024-10-18 15:03:33 +0900

    design: 램프 생성 수정된 디자인 반영

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/registration/RegistrationScreen.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/registration/RegistrationScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
@@ -30,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -115,7 +117,8 @@ fun RegistrationScreen(modifier: Modifier, navController: NavController) {
                 },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(4.dp),
+                    .height(4.dp)
+                    .clip(RoundedCornerShape(50)),
                 color = LightGray,
                 trackColor = Gray
             )
