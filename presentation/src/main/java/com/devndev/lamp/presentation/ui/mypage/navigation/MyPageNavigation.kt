4fc79a655c74527810e1b52658c17d1792dc9b0c
soohwan-k <newpoo@naver.com>
2024-10-22 16:13:45 +0900

    feat: 프로필 수정 화면 구현

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/mypage/navigation/MyPageNavigation.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/mypage/navigation/MyPageNavigation.kt
@@ -1,5 +1,7 @@
 package com.devndev.lamp.presentation.ui.mypage.navigation
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
@@ -9,6 +11,7 @@ import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.devndev.lamp.presentation.ui.common.Route
 import com.devndev.lamp.presentation.ui.mypage.MyPageScreen
+import com.devndev.lamp.presentation.ui.mypage.ProfileEditScreen
 
 fun NavController.navigateMyPage(navOptions: NavOptions? = null) {
     this.navigate(Route.MYPAGE, navOptions)
@@ -16,9 +19,28 @@ fun NavController.navigateMyPage(navOptions: NavOptions? = null) {
 
 fun NavGraphBuilder.myPageNavGraph(
     padding: PaddingValues,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    navController: NavController
 ) {
     composable(Route.MYPAGE) {
-        MyPageScreen(modifier = modifier.padding(padding))
+        MyPageScreen(modifier = modifier.padding(padding), navController = navController)
+    }
+}
+
+fun NavController.navigateProfileEdit(navOptions: NavOptions? = null) {
+    this.navigate(Route.PROFILE_EDIT, navOptions)
+}
+
+fun NavGraphBuilder.profileEditNavGraph(
+    padding: PaddingValues,
+    modifier: Modifier = Modifier,
+    navController: NavController
+) {
+    composable(
+        Route.PROFILE_EDIT,
+        enterTransition = { slideInHorizontally(initialOffsetX = { it }) },
+        exitTransition = { slideOutHorizontally(targetOffsetX = { it }) }
+    ) {
+        ProfileEditScreen(modifier = modifier.padding(padding), navController = navController)
     }
 }
--- presentation/src/main/java/com/devndev/lamp/presentation/ui/registration/ProfileScreen.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/registration/ProfileScreen.kt
@@ -6,20 +6,12 @@ import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,13 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.canhub.cropper.CropImage.CancelledResult.uriContent
@@ -44,12 +31,10 @@ import com.canhub.cropper.CropImageContractOptions
 import com.canhub.cropper.CropImageOptions
 import com.devndev.lamp.presentation.R
 import com.devndev.lamp.presentation.ui.common.LampBigTextField
+import com.devndev.lamp.presentation.ui.common.ProfileImage
 import com.devndev.lamp.presentation.ui.common.SelectionScreen
 import com.devndev.lamp.presentation.ui.common.TwoButtonPopup
-import com.devndev.lamp.presentation.ui.theme.Gray
-import com.devndev.lamp.presentation.ui.theme.ManColor
 import com.devndev.lamp.presentation.ui.theme.Typography
-import com.devndev.lamp.presentation.ui.theme.WomanColor
 
 @Composable
 fun ProfileScreen(
@@ -171,96 +156,8 @@ fun ProfileScreen(
                 profileQuery = it
                 onProfileIntroChange(it)
             },
-            hintText = "자신에 대해 소개해주세요",
+            hintText = stringResource(id = R.string.introduce_profile_hint),
             maxLength = maxProfileChar
-        )
-    }
-}
-
-@Composable
-fun ProfileImage(
-    bitmap: Bitmap?,
-    onClick: () -> Unit,
-    onDelete: () -> Unit,
-    isFirstImage: Boolean = false
-) {
-    val gradientBrush = Brush.horizontalGradient(
-        colors = listOf(WomanColor, ManColor)
-    )
-
-    val modifier = Modifier
-        .size(95.dp)
-        .clip(RoundedCornerShape(4.dp))
-        .background(Gray)
-        .then(
-            if (isFirstImage) {
-                Modifier.border(
-                    width = 1.dp,
-                    brush = gradientBrush,
-                    shape = RoundedCornerShape(4.dp)
-                )
-            } else {
-                Modifier.border(width = 1.dp, color = Gray)
-            }
-        )
-        .clickable(onClick = onClick)
-
-    Box(modifier = modifier) {
-        Icon(
-            modifier = Modifier.align(Alignment.Center),
-            painter = painterResource(id = R.drawable.image_icon),
-            contentDescription = null,
-            tint = Color.Unspecified
-        )
-        if (bitmap != null) {
-            Image(
-                bitmap = bitmap.asImageBitmap(),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = modifier
-            )
-
-            if (isFirstImage) {
-                MainProfileImage()
-            }
-
-            Box(
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .size(24.dp)
-                    .background(color = Color.Transparent)
-                    .clickable(onClick = onDelete)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.x_button),
-                    contentDescription = null,
-                    tint = Color.White,
-                    modifier = Modifier.size(20.dp)
-                )
-            }
-        } else {
-            if (isFirstImage) {
-                MainProfileImage()
-            }
-        }
-    }
-}
-
-@Composable
-fun MainProfileImage() {
-    Box(
-        modifier = Modifier
-            .size(width = 41.dp, height = 26.dp)
-            .clip(RoundedCornerShape(topStart = 4.dp, bottomEnd = 4.dp))
-            .background(
-                color = WomanColor
-            )
-    ) {
-        Text(
-            text = stringResource(id = R.string.representative),
-            color = Color.White,
-            modifier = Modifier.align(Alignment.Center),
-            style = Typography.normal12
         )
     }
 }
