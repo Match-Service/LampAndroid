9299730199fbc2709d7d23b40535913023daf3f1
soohwan-k <newpoo@naver.com>
2024-11-12 18:11:27 +0900

    fix: ReviewProgressBar 와 하단 버튼 사이 간격 조정

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/review/LampReviewScreen.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/review/LampReviewScreen.kt
@@ -91,11 +91,11 @@ fun LampReviewScreen() {
                     modifier = Modifier.wrapContentSize()
                 ) {
                     Spacer(modifier = Modifier.weight(1f))
+                    ReviewProgressBar(progress) { newValue ->
+                        progress = newValue
+                    }
+                    Spacer(modifier = Modifier.height(73.dp))
                 }
-                ReviewProgressBar(progress) { newValue ->
-                    progress = newValue
-                }
-                Spacer(modifier = Modifier.height(73.dp))
             }
         }
     }
@@ -150,7 +150,7 @@ fun ReviewProgressBar(progress: Float, onProgressChange: (Float) -> Unit) {
                 valueRange = 25f..100f,
                 steps = 0,
                 modifier = Modifier
-                    .fillMaxSize(),
+                    .fillMaxWidth(),
                 thumb = {
                     Image(
                         painter = painterResource(id = R.drawable.heart),
