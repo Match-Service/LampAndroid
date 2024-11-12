926d2862176c761227330809f2212e7e8a588cb8
ej_lee <leedjwls@naver.com>
2024-11-12 20:47:21 +0900

    fix: slide progress bar 구분선이 가리는 현상 수정

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/review/LampReviewScreen.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/review/LampReviewScreen.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -39,6 +41,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -110,10 +113,36 @@ fun ReviewProgressBar(progress: Float, onProgressChange: (Float) -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Box(
+        BoxWithConstraints(
             modifier = Modifier.fillMaxWidth()
         ) {
-            // Draw dividers
+            // 슬라이더
+            Slider(
+                value = progress,
+                onValueChange = { newValue ->
+                    onProgressChange(
+                        when {
+                            newValue < 38 -> 25f
+                            newValue < 63 -> 50f
+                            newValue < 88 -> 75f
+                            else -> 100f
+                        }
+                    )
+                },
+                valueRange = 25f..100f,
+                steps = 0,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .zIndex(0f),
+                thumb = {},
+                colors = SliderDefaults.colors(
+                    thumbColor = Color.White,
+                    activeTrackColor = Color.White,
+                    inactiveTrackColor = Gray
+                )
+            )
+
+            // 구분선 그리기
             Canvas(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -134,35 +163,18 @@ fun ReviewProgressBar(progress: Float, onProgressChange: (Float) -> Unit) {
                 }
             }
 
-            // 슬라이더
-            Slider(
-                value = progress,
-                onValueChange = { newValue ->
-                    onProgressChange(
-                        when {
-                            newValue < 38 -> 25f
-                            newValue < 63 -> 50f
-                            newValue < 88 -> 75f
-                            else -> 100f
-                        }
-                    )
-                },
-                valueRange = 25f..100f,
-                steps = 0,
+            // thumb(하트) 따로 그리기
+            Image(
+                painter = painterResource(id = R.drawable.heart),
+                contentDescription = null,
                 modifier = Modifier
-                    .fillMaxWidth(),
-                thumb = {
-                    Image(
-                        painter = painterResource(id = R.drawable.heart),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                colors = SliderDefaults.colors(
-                    thumbColor = Color.White,
-                    activeTrackColor = Color.White,
-                    inactiveTrackColor = Gray
-                )
+                    .size(24.dp)
+                    .align(Alignment.CenterStart)
+                    .offset {
+                        val offsetX = ((progress - 25f) / (100f - 25f) * (constraints.maxWidth - 24.dp.toPx())).toInt()
+                        IntOffset(offsetX, 0)
+                    }
+                    .zIndex(3f)
             )
         }
         Row(
