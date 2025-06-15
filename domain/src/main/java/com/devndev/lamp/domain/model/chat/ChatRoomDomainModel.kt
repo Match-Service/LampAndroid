eb2f7cf5bcd3fc50aba26630581fc047000dd72e
soohwan-k <newpoo@naver.com>
2025-06-15 14:09:23 +0900

    feat: 채팅 리스트 화면에서 마지막 메시지 약속  채팅일 경우 표시 변경

--- domain/src/main/java/com/devndev/lamp/domain/model/chat/ChatRoomDomainModel.kt
+++ domain/src/main/java/com/devndev/lamp/domain/model/chat/ChatRoomDomainModel.kt
@@ -15,8 +15,11 @@ data class ChatRoomDomainModel(
 data class LastMessageInfo(
     val userName: String,
     val message: String,
-    val createdAt: String
-)
+    val createdAt: String,
+    val messageType: String
+) {
+    val messageTypeEnum: MessageType? get() = MessageType.fromString(messageType)
+}
 
 data class ChatAppointment(
     val appointmentDate: String,
