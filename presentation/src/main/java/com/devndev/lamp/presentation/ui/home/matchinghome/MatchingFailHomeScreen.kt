0a090fdfe8d7957e64cbb663f16785f43791d29b
ej_lee <leedjwls@naver.com>
2025-07-26 13:51:15 +0900

    feat: 매칭 실패 화면 수정중

--- presentation/src/main/java/com/devndev/lamp/presentation/ui/home/matchinghome/MatchingFailHomeScreen.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/home/matchinghome/MatchingFailHomeScreen.kt
@@ -1,6 +1,7 @@
 package com.devndev.lamp.presentation.ui.home.matchinghome
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
@@ -236,22 +237,25 @@ fun MatchingFailHomeScreen(
 
                     Spacer(modifier = Modifier.height(69.dp))
 
-                    LampButton(
-                        isGradient = !state.isMatching,
-                        buttonWidth = 156,
-                        buttonText = reMatching,
-                        onClick = {
-                            // startMatch 하면 userStatus = MATCHING
-                            viewModel.startMatch()
-                        },
-                        enabled = state.isOwner
-                    )
+                    Log.e("state.isOwner", "${state.isOwner}")
+                    if (state.isOwner) {
+                        LampButton(
+                            isGradient = !state.isMatching,
+                            buttonWidth = 156,
+                            buttonText = reMatching,
+                            onClick = {
+                                // startMatch 하면 userStatus = MATCHING
+                                viewModel.startMatch()
+                            },
+                            enabled = true
+                        )
+                    }
                     LampButton(
                         isGradient = false,
                         buttonWidth = 156,
                         buttonText = cancelMatching,
                         onClick = {
-                            viewModel.stopMatch()
+//                            navController.navigateMatching()
                         },
                         enabled = true
                     )
--- presentation/src/main/java/com/devndev/lamp/presentation/ui/home/navigation/HomeNavigation.kt
+++ presentation/src/main/java/com/devndev/lamp/presentation/ui/home/navigation/HomeNavigation.kt
@@ -8,8 +8,9 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.devndev.lamp.presentation.ui.common.Route
-import com.devndev.lamp.presentation.ui.home.findlamp.FindLampScreen
 import com.devndev.lamp.presentation.ui.home.main.HomeScreen
+import com.devndev.lamp.presentation.ui.home.matchinghome.MatchingHomeScreen
+import kotlinx.coroutines.flow.SharedFlow
 
 fun NavController.navigateHome(navOptions: NavOptions? = null) {
     this.navigate(Route.HOME, navOptions)
@@ -38,20 +39,27 @@ fun NavGraphBuilder.homeNavGraph(
     }
 }
 
-fun NavController.navigateFind(
+fun NavController.navigateMatching(
     navOptions: NavOptions? = null
 ) {
-    this.navigate(Route.FIND, navOptions)
+    this.navigate(Route.MATCHING, navOptions)
 }
 
-fun NavGraphBuilder.findNavGraph(
+fun NavGraphBuilder.matchingNavGraph(
     padding: PaddingValues,
     modifier: Modifier = Modifier,
-    navController: NavController
+    navController: NavController,
+    updateEvent: SharedFlow<Unit>,
+    updateStatus: SharedFlow<String>
 ) {
     composable(
         Route.FIND
     ) {
-        FindLampScreen(modifier = modifier.padding(padding), navController = navController)
+        MatchingHomeScreen(
+            modifier = modifier.padding(padding),
+            navController = navController,
+            updateEvent = updateEvent,
+            updateStatus = updateStatus
+        )
     }
 }
